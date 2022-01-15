b3bf8a2565eb93ca9db9e0755c765cf75f0ff763
grinaldyafi <grinaldy.yafi@mail.ugm.ac.id>
2022-01-15 19:17:20 +0700

    change desc

--- resources/views/koleksi-buku.blade.php
+++ resources/views/koleksi-buku.blade.php
@@ -18,7 +18,7 @@
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
         <h1 class="font-weight-bold">AkuSaranin - BacaBuku 📚</h1>
-        <p class="lead text-muted">Rekombuk merupakan platform koleksi buku rekomendasi. Tidak hanya mencari buku rekomendasi, di sini kamu juga dapat menambah buku yang kamu rekomendasikan.<br><br> <strong>Tunggu apalagi? Kontribusimu sangat kami tunggu 😎👇</strong><p>
+        <p class="lead text-muted">AkuSaranin merupakan platform koleksi buku rekomendasi. Tidak hanya mencari buku rekomendasi, di sini kamu juga dapat menambah buku yang kamu rekomendasikan.<br><br> <strong>Tunggu apalagi? Kontribusimu sangat kami tunggu 😎👇</strong><p>
           <a href="{{url('/koleksi-buku')}}" class="btn btn-primary my-2">Koleksi Buku</a>
           <a href="{{url('/tambah-buku')}}"class="btn btn-secondary my-2">Kasih Rekomendsi Buku</a>
         </p>
